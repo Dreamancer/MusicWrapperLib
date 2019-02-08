e283f84ef692b6ed15d3e6d6eeb79df9e63e4ca0
Dreamancer <nambdull@gmail.com>
2019-02-08 22:17:49 +0100

    formating

--- MusicWrapper/MusicWrapper/ApiWrappers/ApiResponse.cs
+++ MusicWrapper/MusicWrapper/ApiWrappers/ApiResponse.cs
@@ -4,10 +4,10 @@ using Newtonsoft.Json.Linq;
 namespace MusicWrapper
 {
 	public class ApiResponse
-    {
-        public ApiName ApiName { get; set; }
-        public ResponseType ResponseType { get; set; }
-        public JObject Response { get; set; }
+	{
+		public ApiName ApiName { get; set; }
+		public ResponseType ResponseType { get; set; }
+		public JObject Response { get; set; }
 		public string Message { get; set; }
-    }
+	}
 }
--- MusicWrapper/MusicWrapper/ApiWrappers/IMusicApiWrapper.cs
+++ MusicWrapper/MusicWrapper/ApiWrappers/IMusicApiWrapper.cs
@@ -3,19 +3,19 @@
 namespace MusicWrapper.Wrappers
 {
 	interface IMusicApiWrapper
-  {
-    Task<ApiResponse> SearchArtists(string name);
+	{
+		Task<ApiResponse> SearchArtists(string name);
 
-    Task<ApiResponse> GetArtistInfo(string id);
+		Task<ApiResponse> GetArtistInfo(string id);
 
-    Task<ApiResponse> GetArtistAlbums(string id);
+		Task<ApiResponse> GetArtistAlbums(string id);
 
-    Task<ApiResponse> GetAlbumInfo(string id);
+		Task<ApiResponse> GetAlbumInfo(string id);
 
-    //ApiResponse SearchSongs(string name);
+		//ApiResponse SearchSongs(string name);
 
-    Task<ApiResponse> GetSongInfo(string id);
+		Task<ApiResponse> GetSongInfo(string id);
 
-    Task<ApiResponse> GetArtistEvents(string artistId);
-  }
+		Task<ApiResponse> GetArtistEvents(string artistId);
+	}
 }
